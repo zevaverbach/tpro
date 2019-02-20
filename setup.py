8782a0fda007305cc6a74014cf571adebd1f9a39
zevav <zev@averba.ch>
2019-02-20 09:03:05 -0500

    bumped setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setup(
     name="tpro",
-    version="0.04",
+    version="0.05",
     url='https://github.com/zevaverbach/tpro',
     install_requires=[
         'Click',
